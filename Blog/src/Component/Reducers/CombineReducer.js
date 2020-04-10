362ddd5a5bf96d1c433c9e62e3e4e6829a564255
Ye Wang <yewang2019@github.com>
2020-04-10 13:13:54 -0400

    routing config ,reducer

--- Blog/src/Component/Reducers/CombineReducer.js
+++ Blog/src/Component/Reducers/CombineReducer.js
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
 
+const lineReducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return null;
+    }
+}
 export default combineReducers({
-    haha: ""
+    test: lineReducer
 });
--- Blog/src/index.js
+++ Blog/src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+import { Provider } from 'react-redux';
+import { Router, Route, Switch } from 'react-router'
+import combineReduer from './Component/Reducers/combineReducer'
 import App from './Component/App';
+// import createBrowserHistory from "./Config/History"
+import { createBrowserHistory } from "history";
+import { compose } from "redux";
+
+
+
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const store = createStore(
+  combineReduer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={createBrowserHistory()}>
+      < Switch >
+        < App />
+
+      </Switch >
+    </Router>
+  </Provider>,
+
   document.getElementById('root')
 );
